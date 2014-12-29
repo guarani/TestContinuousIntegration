7935d7c43ab37ab7015639dd70762b36389d06af
Paul Von Schrottky <pvonschrottky@roshka.com>
2014-12-29 17:59:08 -0300

    Fixed test

--- TestContinuousIntegrationTests/TestContinuousIntegrationTests.swift
+++ TestContinuousIntegrationTests/TestContinuousIntegrationTests.swift
@@ -23,7 +23,7 @@ class TestContinuousIntegrationTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true && false, "Pass")
+        XCTAssert(true, "Pass")
     }
     
     func testPerformanceExample() {
